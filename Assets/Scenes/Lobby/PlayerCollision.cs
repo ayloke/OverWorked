0dcc615511bb84af24527160791ffb439521c6b1
ayloke <ayloke@bk.ru>
2023-11-20 23:34:10 +0300

    add URP

--- Assets/Scenes/Lobby/PlayerCollision.cs
+++ Assets/Scenes/Lobby/PlayerCollision.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public Movement movement;
-    void OnCollisionEnter (Collision collisionInfo)
+    void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "TableMenu")
         {
